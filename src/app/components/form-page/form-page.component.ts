b5aa6358eb3c30e31a64e85320cb9bd7cc583e74
Wutthiphon Tassana <woody@wutthiphon.space>
2025-09-15 17:02:24 +0700

    Add form fields and validation to FormPageComponent

--- src/app/components/form-page/form-page.component.ts
+++ src/app/components/form-page/form-page.component.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './form-page.component.scss',
 })
 export class FormPageComponent {
+  form_data = {
+    username: '',
+    password: '',
+    re_password: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    agree: false,
+  };
+  form_error_message: string = '';
+
   constructor() {}
+
+  onSubmit() {
+    const { password, re_password, agree } = this.form_data;
+
+    if (password != re_password) {
+      this.form_error_message = 'รหัสผ่านไม่ตรงกัน';
+      return;
+    }
+
+    if (!agree) {
+      this.form_error_message = 'คุณไม่ยอมรับเงื่อนไข';
+      return;
+    }
+
+    console.log(this.form_data);
+  }
 }
